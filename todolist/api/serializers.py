341525e89859ac37a4fe4dc4f518ebceba215046
YeuVoBan14 <nhathung142002@gmail.com>
2024-04-13 12:07:14 +0700

    done

--- todolist/api/serializers.py
+++ todolist/api/serializers.py
@@ -9,3 +9,22 @@ class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = '__all__'
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    def validate_old_password(self, value):
+        user_id = self.context.get('user_id')
+        try:
+            user = User.objects.get(pk=user_id)
+        except (User.DoesNotExist, ValueError):
+            raise serializers.ValidationError("User not found or invalid ID")
+
+        if not user.check_password(value):
+            raise serializers.ValidationError("Incorrect old password")
+        return value
+    
+#     {
+#     "old_password": "current_password",
+#     "new_password": "new_password"
+# }
--- todolist/api/views.py
+++ todolist/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from home.models import User, Job
-from .serializers import JobSerializer, UserSerializer
+from .serializers import JobSerializer, UserSerializer, ChangePasswordSerializer
+from datetime import datetime, timezone
+from django.contrib.auth import authenticate, login
 
 @api_view(['GET'])
 def getRoute(request):
@@ -35,7 +37,10 @@ def getJob(request, pk):
     return Response(serializer.data)
 @api_view(['POST'])
 def createJob(request):
-    serializer = JobSerializer(data=request.data)
+    if isinstance(request.data, list):
+        serializer = JobSerializer(data=request.data, many=True)
+    else:
+        serializer = JobSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
@@ -51,4 +56,90 @@ def deleteJob(request, pk):
     job = Job.objects.get(pk=pk)
     job.delete()
     return Response('Deleted Successfully!')
+@api_view(['GET'])
+def filterJobs(request):
+  keyword = request.GET.get('keyword')
+  if keyword:
+    filtered_jobs = Job.objects.filter(title__icontains=keyword)
+  else:
+    filtered_jobs = Job.objects.all()
+
+  serializer = JobSerializer(filtered_jobs, many=True)
+  return Response(serializer.data)
+@api_view(['GET'])
+def filterDateJobs(request):
+  startdate  = request.GET.get('startdate')
+  if startdate:
+    try:
+      startdate_utc = datetime.strptime(startdate, '%Y-%m-%d').replace(tzinfo=timezone.utc)
+    except ValueError:
+      return Response('Invalid start date format')
+  if startdate:
+    filtered_jobs = Job.objects.filter(date_posted__gte=startdate)
+  else:
+    filtered_jobs = Job.objects.all()
+
+  serializer = JobSerializer(filtered_jobs, many=True)
+  return Response(serializer.data)
+
+@api_view(['POST'])
+def createUser(request):
+    if isinstance(request.data, list):
+        serializer = UserSerializer(data=request.data, many=True)
+    else:
+        serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+@api_view(['PUT'])
+def updateUser(request, pk):
+    user = User.objects.get(pk=pk)
+    serializer = UserSerializer(instance=user, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+@api_view(['DELETE'])
+def deleteUser(request, pk):
+    user = User.objects.get(pk=pk)
+    user.delete()
+    return Response('Deleted Successfully!')
+@api_view(['POST'])
+def changePassword(request, pk):
+    serializer = ChangePasswordSerializer(data=request.data, context={'user_id': pk})
+    if serializer.is_valid():
+        user = User.objects.get(id=pk)
+        user.set_password(serializer.data['new_password'])
+        user.save()
+        return Response('Password changed successfully')
+    else:
+        errors = serializer.errors
+        return Response(errors)
+@api_view(['POST'])
+def loginPage(request):
+    data = request.data
+    username = data.get('username')
+    password = data.get('password')
+
+    if username is None or password is None:
+        return Response('Missing username or password')
+    user = authenticate(request, username=username, password=password)
+    if user is None:
+        return Response('Invalid username or password')
+    else:
+       login(request, user)
+       return Response('Login successfully')
+#     {
+#   "username": "your_username",
+#   "password": "your_password"
+# }
+@api_view(['GET'])
+def getJobswithApplicants(request):
+    jobs_with_applicants = Job.objects.filter(applicants__isnull=False).prefetch_related('applicants')
+    serializer = JobSerializer(jobs_with_applicants, many=True)
+    if not serializer.data:
+        return Response('No jobs found with applicants')
+    for job in serializer.data:
+        job['applicants'] = [User.objects.get(pk=applicant).username for applicant in job['applicants']]
+    return Response(serializer.data)
+
     
